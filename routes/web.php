2a6f88bf509f61fac1f9265a8f900034e7d8734e
Aditya <Adityadi230@gmail.com>
2020-08-10 03:01:14 +0700

    pertemuan 10

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('foo', function () {
-    return 'Hello World';
+Route::get('home', function () {
+    return view('home');
 });
