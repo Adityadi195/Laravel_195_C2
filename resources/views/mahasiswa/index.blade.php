c99a828cd88ac3c02b3d447a0dfaad1219f66b38
Aditya <Adityadi230@gmail.com>
2020-08-10 05:00:40 +0700

    pertemuan 11 : install Bootstrap

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Halaman Mahasiswa')
+@section('bread1', 'Mahasiswa')
+@section('bread2', 'Daftar')
+
+
+@section('content')
+<p>this is my body.</p>
+
+@endsection
